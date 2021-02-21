a7cad16d52b26785fdedf8608b75ca3449949fa1
jhonatanferreiraoliveira <54809009+jhonatanferreiraoliveira@users.noreply.github.com>
2021-02-21 12:20:45 -0300

    Inicio

---
+++ content/js/padrao.js
@@ -0,0 +1,10 @@
+$(document).on('ready', function () {
+    $('.slide-marcas').slick({
+        slidesToShow: 6,
+        slidesToScroll: 3,
+        dots: true,
+        arrows: false,
+        focusOnSelect: true,
+        Infinity: true
+    });
+});
