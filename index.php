02826f8f7a9580d3ef19d35dade84c9cd1aac16a
jhonatanferreiraoliveira <54809009+jhonatanferreiraoliveira@users.noreply.github.com>
2021-03-03 01:02:57 -0300

    hover

--- index.php
+++ index.php
@@ -26,8 +26,6 @@
   <!-- Conteudo -->
   <?php include 'footer.php';?>
   <!-- Conteudo -->
-
-  
    
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="content/js/bootstrap.min.js"></script>
